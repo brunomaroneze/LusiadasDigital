d4dee57e1bf7560caad42d909f3ab6252183e5e2
Murilo Esteves <muriloesteves6@gmail.com>
2025-12-10 03:42:13 -0300

    Branch apenas para teste, a pasta LusiadasTextos não teve mudanças.

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,8 @@
-# homepage/urls.py
-
 from django.urls import path
 from . import views
 
-app_name = 'homepage' # É uma boa prática definir um namespace para apps
-
 urlpatterns = [
-    path('', views.index, name='index'), # 'name' é o nome da URL que você usará nos templates
+    path('', views.home, name='home'),  # Caminho Raiz
+    path('sobre/', views.sobre, name='sobre'),  # Caminho informações
+    path('autor/', views.autor, name='autor'),  # Caminho Autor
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
 
-def index(request):
-    return render(request, 'homepage/index.html')
+def home(request):
+    return render(request, 'homepage.html')
+
+
+def autor(request):
+    return render(request, 'autor.html')
+
+
+def sobre(request):
+    return render(request, 'sobre.html')
